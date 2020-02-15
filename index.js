f5a260da02ecc16e47be491de9d1dc5e78370d4d
Jeffrey Rehm <47900191+j-rehm@users.noreply.github.com>
2020-02-14 18:50:57 -0700

    Hello World
    NOTE:
    We are using port 3000 at the moment. This will change when we put the site on aws.
    Run 'init.bat' before running the server. This will install all needed node packages and dependencies listed in 'package.json'

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const expressSession = require('express-session');
+const pug = require('pug');
+const bodyParser = require('body-parser');
+const path = require('path');
+const bcrypt = require('bcrypt-nodejs');
+const routes = require('./routes/routes.js');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname + '/views'));
+app.use(express.static(path.join(__dirname + '/public')));
+// app.use(cookieParser('login'));
+
+app.get('/', routes.index);
+
+app.listen(3000);
