388137ed6080f891de5f6e9bb8ee50fc74f1fa30
WistyDagel <chrisdazley@gmail.com>
2020-02-19 17:52:24 -0700

    Multiple changes
    Created public folder, created a layout page, edited the index page

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,8 @@
 exports.index = (req, res) => {
-    res.render('index');
+    res.render('index', {
+        "title": "Login",
+        "username": "Username: ",
+        "password": "Password: ",
+        "noAccount": "Don't have an account? Click here!"
+    });
 }
