f9dca7cd1b36116af9c31e430911924fdf98014f
Jeffrey Rehm <47900191+j-rehm@users.noreply.github.com>
2020-02-26 17:38:37 -0700

    Put the mongo-atlas code into a dedicated javascript file and reorganized routes.js to match index.js calls
    database.js is where the board-database interfacing will be done.
    database.js code has been documented properly

--- index.js
+++ index.js
@@ -37,11 +37,11 @@ const checkAuth = (req, res, next) => {
 
 // Routes
 app.get('/', routes.index);
-app.get('/board', routes.board);
 app.post('/login', urlEncodedParser, routes.validateLogin);
 app.get('/create', routes.create);
 app.post('/create', urlEncodedParser, routes.parseCreateData);
 app.get('/home', checkAuth, routes.home);
+app.get('/board', routes.board);
 app.get('/edit', routes.edit);
 
 app.listen(3000);
