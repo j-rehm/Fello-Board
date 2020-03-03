e3eae9bf6be815168f0b7c31bde2b93e0012fb0d
Jeffrey Rehm <47900191+j-rehm@users.noreply.github.com>
2020-03-02 19:28:40 -0700

    Board saving

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(expressSession({
     resave: true
 }));
 
-app.use(express.json());
+// app.use(express.json());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
--- public/scripts/boardSaving.js
+++ public/scripts/boardSaving.js
@@ -2,27 +2,22 @@ const saveButton = document.getElementById('saveButton');
 const xhr = new XMLHttpRequest();
 
 const sendBoardData = () => {
-    let boardId = JSON.parse(document.getElementById('boardId').value).boardId;
+    // let boardId = JSON.parse(document.getElementById('boardId').value).boardId;
     let boardData = document.getElementById('parentDiv');
     let stringBoardData = new XMLSerializer().serializeToString(boardData);
 
     console.log("Sending board data...");
 
-    // xhr.open("POST", '/board');
-    // xhr.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
-    // xhr.send(stringBoardData);
-
-    console.log({
-        id: boardId,
-        boardData: stringBoardData
-    });
-
     xhr.open("POST", '/updateBoard');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-        id: boardId,
-        boardData: stringBoardData
-    }));
+    xhr.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
+    xhr.send(stringBoardData);
+
+    // xhr.open("POST", '/updateBoard');
+    // xhr.setRequestHeader('Content-Type', 'application/json');
+    // xhr.send(JSON.stringify({
+    //     id: boardId,
+    //     boardData: stringBoardData
+    // }));
 };
 
 saveButton.onclick = sendBoardData;
